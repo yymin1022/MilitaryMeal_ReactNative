2f0dbb03bba1fab61687533867d0a1dd5a40c39e
yymin1022 <Yymin1022@gmail.com>
2021-06-18 00:52:39 +0900

    Update RankActivity Layout

--- App.js
+++ App.js
@@ -5,7 +5,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.containerAppBar}>
+        <Text>Title Bar</Text>
+      </View>
+      <View style={styles.containerTop}>
+        <View style={styles.containerTopItem}>
+          <View style={styles.containerTopItemSide}>
+            <Text>Rank 2</Text>
+          </View>
+        </View>
+        <View style={styles.containerTopItem}>
+          <View style={styles.containerTopItemCenter}>
+            <Text>Rank 1</Text>
+          </View>
+        </View>
+        <View style={styles.containerTopItem}>
+          <View style={styles.containerTopItemSide}>
+            <Text>Rank 3</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.containerBottom}>
+        <Text>Other Ranks</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +36,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+
     alignItems: 'center',
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+  },
+  containerAppBar: {
+    flex: 1,
+
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+  },
+  containerTop: {
+    flex: 4,
+    flexDirection: 'row',
+
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    margin: 5
+  },
+  containerTopItem: {
+    height: "100%",
+    flex: 1,
+    
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+  },
+  containerTopItemCenter: {
+    flex: 1,
+    height: '100%',
+    width: '90%',
+    
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    borderRadius: 10,
+    justifyContent: 'center'
+  },
+  containerTopItemSide: {
+    flex: 1,
+    height: '100%',
+    width: '90%',
+    
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    borderRadius: 10,
+    justifyContent: 'center',
+    marginTop: 10
+  },
+  containerBottom: {
+    flex: 10,
+
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
     justifyContent: 'center',
   },
 });
