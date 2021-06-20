6dd2b30b37ae554d90d35e7085319834e73e5f2b
yymin1022 <Yymin1022@gmail.com>
2021-06-20 15:45:42 +0900

    Cleaned UP

--- RankItem.js
+++ RankItem.js
@@ -2,29 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function RankItem(props) {
-  const menuRank = props.rank;
-  const menuTitle = props.menu;
+    const menuRank = props.rank;
+    const menuTitle = props.menu;
 
-  return (
-    <View style={styles.container}>
-        
-      <Text>{menuRank}</Text>
-      <Text>{menuTitle}</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+            <Text>{menuRank}</Text>
+            <Text>{menuTitle}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    height: '100%',
-    width: '95%',
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        height: '100%',
+        width: '95%',
 
-    alignItems: 'center',
-    backgroundColor: 'lightgray',
-    borderRadius: 10,
-    justifyContent: 'center',
-    margin: 8
-  },
+        alignItems: 'center',
+        backgroundColor: 'lightgray',
+        borderRadius: 10,
+        justifyContent: 'center',
+        margin: 8
+    },
 });
